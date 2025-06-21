75a2ab1dc23fa8f9e401ad328262e6ae805a287e
Dan Bagrin <bagrin.danu@gmail.com>
2025-06-21 15:47:29 +0300

    mentorGPT: Init

---
+++ ProjectSigmoind.Domain/Models/PromntModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSigmoind.Domain.Models {
+    public class PromntModel {
+        public string UserPromnt { get; set; }
+    }
+}
