276659a1513c314a6d0eeac5f53fc9b0ac35414f
Dan Bagrin <bagrin.danu@gmail.com>
2025-06-21 15:47:02 +0300

    MentorGPT: Init

---
+++ ProjectSigmoind.BussinesLayer/Enitity/User/User.cs
@@ -0,0 +1,21 @@
+﻿using ProjectSigmoind.BussinesLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSigmoind.Domain.Entitys;
+
+namespace ProjectSigmoind.BussinesLayer.Enitity.User {
+    public class User {
+        private readonly UserEntity user;
+
+        public User(string UserName) { 
+            this.Name = UserName;
+        }
+
+        // Action
+        
+
+    }
+}
---
+++ ProjectSigmoind.BussinesLayer/Interfaces/IUserInterface.cs
@@ -0,0 +1,12 @@
+﻿using ProjectSigmoind.BussinesLayer.Enitity.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSigmoind.BussinesLayer.Interfaces {
+    public interface IUserInterface {
+        string userActionPromnt(User userPromnt);
+    }
+}
